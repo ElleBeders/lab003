7106490a6de966aa1bc1a5d20b588ac125a92a48
ElleBeders <malefisenta135@gmail.com>
2026-03-13 06:20:57 +0500

    Add files via upload

---
+++ 001/ConsoleApplication6/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+// Сисок пользователя
+let rec readR acc =
+    let ch = Console.ReadLine()
+    if ch = "000" then
+        List.rev acc
+    else
+        readR (int(ch) :: acc) 
+
+let oneNumber (digits: seq<int>) : int =
+    digits
+    |> Seq.fold (fun acc digit ->
+        if digit % 2 = 0 then     
+            acc * 10 + digit  
+        else
+            acc              
+    ) 0  
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите цифры (по одной):"
+    printfn "(000 — конец ввода)"
+    
+    let number = readR []
+    let result = oneNumber (seq number) 
+    
+    printfn "Исходные цифры: %A" number
+    printfn "Число из чётных цифр: %d" result
+    0
